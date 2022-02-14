a4fee0b1d482fe89be4452282fdc9d8218d7c4c8
Arjun Nemani <nemaniarjun@gmail.com>
2022-02-14 22:44:48 +0530

    Support html files rendering from src/public folder

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import * as controllers from "./controllers";
 
 const app = express();
+const path = require("path");
 
 // Enable CORS
 app.use(function (req, res, next) {
@@ -12,18 +13,7 @@ app.use(function (req, res, next) {
 });
 
 app.get("/", (req, res) => {
-  res.send(
-    `
-    <h1>Welcome to Perp.exchange API for coingecko.</h1>
-    <h2>This was created by
-    <a href="https://github.com/nanspro">@nanspro</a>,
-    <a href="https://nemani.dev">@nemani</a>
-    and
-    <a href="https://github.com/sounak98">@sounak98</a> </h2>
-    <h3> <a href="/pairs"> Click to access api</a> </h3>
-    <h3> <a href="mailto:perp.gq@nemani.dev"> Click to report bugs </a> </h3>
-    <h3> <a href="https://github.com/sounak98/perp-coingecko-api"> Click to view code </a> </h3>`
-  );
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.get("/pairs", controllers.getPairs);
