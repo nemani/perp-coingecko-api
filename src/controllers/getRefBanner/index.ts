f4939749117a8375a6daba6e4acf37dfff4771c4
Arjun Nemani <nemaniarjun@gmail.com>
2022-02-25 15:44:58 +0530

    add type=svg for debug

--- src/controllers/getRefBanner/index.ts
+++ src/controllers/getRefBanner/index.ts
@@ -6,6 +6,8 @@ import square from "./square";
 const getRefBanner = async (req, res, next) => {
   const shape: string = req.query.shape || "square";
   const refCode: string = req.query.refCode;
+  const type: string = req.query.type;
+
   const QrPngString = await QRCode.toDataURL(
     `https://app.perp.com/?referral=${refCode}`
   );
@@ -21,12 +23,18 @@ const getRefBanner = async (req, res, next) => {
   }
 
   const resultSvg = template(data);
-  const resultPng = await sharp(Buffer.from(resultSvg))
-    .toFormat("png")
-    .toBuffer();
 
-  res.set("Content-Type", "image/png");
-  res.send(resultPng);
+  if (type === "svg") {
+    res.set("Content-Type", "text/html");
+    res.send(`<html>${resultSvg}</html>`);
+  } else {
+    const resultPng = await sharp(Buffer.from(resultSvg))
+      .toFormat("png")
+      .toBuffer();
+
+    res.set("Content-Type", "image/png");
+    res.send(resultPng);
+  }
 };
 
 export default getRefBanner;
